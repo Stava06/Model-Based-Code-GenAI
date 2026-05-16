dab2838f0dbf1c88e4952d43c0845c5f15ae9150
OmriSpitzer <130233699+OmriSpitzer@users.noreply.github.com>
2026-05-16 19:54:27 +0300

    asdasdsa

---
+++ appserver/messages.py
@@ -0,0 +1,22 @@
+import time
+TIME = None
+
+def start_message(directory: str, variables: dict = {}):
+    directory = directory.capitalize()
+    print(f" --------- Starting {directory} --------- ")
+    print(f"Variables: {variables}\n")
+
+    global TIME
+    TIME = time.time()
+
+def success_message(directory: str, message: str = None):
+    directory = directory.capitalize()
+    print(f" --------- SUCCESS in {directory} --------- ")
+    if message:
+        print(f"Message: {message}\n")
+    print(f"Time taken: {round(time.time() - TIME, 2)} seconds\n")
+
+def error_message(directory: str, error: str):
+    directory = directory.capitalize()
+    print(f" --------- ERROR in {directory} --------- ")
+    print(f"Error: {error}\n")
