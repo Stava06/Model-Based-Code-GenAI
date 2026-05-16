f86421aecf7629ee2d15a2406ec73f4c65d0ac8c
Stava06 <stav.avraham@e.braude.co.il>
2026-05-16 20:46:21 +0300

    Update UserService.js

--- myapp/my-web/src/components/services/UserService.js
+++ myapp/my-web/src/components/services/UserService.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
 
+// Register user
 export const registerUser = async (fullName, email, password) => {
     const response = await axios.post(`${API_URL}/users/register`, { fullName, email, password });
     console.log(`${response.data}`);
